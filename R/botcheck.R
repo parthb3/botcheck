0a9a16115bf0f4e1d1699706dd68534a103618b3
parthb3 <42756449+parthb3@users.noreply.github.com>
2021-06-28 02:18:41 -0400

    Update botcheck.R

--- R/botcheck.R
+++ R/botcheck.R
@@ -39,7 +39,7 @@ botcheck = function(user) {
   # Make the API request
   result = POST("https://botometer-pro.p.rapidapi.com/2/check_account",
                  encode="json",
-                 add_headers(`X-Mashape-Key`=Mashape_key),
+                 add_headers(`X-RapidAPI-Key`=RapidAPI_key),
                  body=body_json)
   
   # Parse result
